9d7f7e68d156028cdff416f04479ccc7efb2134b
zaratedev <zaratedev@gmail.com>
2017-08-31 13:52:59 -0500

    First commit and configuration of the project

---
+++ webpack.mix.js
@@ -0,0 +1,5 @@
+const { mix } = require('laravel-mix');
+
+mix.js('resources/assets/js/app.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .combine(['resources/assets/css/style.css'],'public/css/style.css');
