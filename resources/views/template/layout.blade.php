82407e347c3dc633c89b5b50bc2c7de86740c663
Jonathan Zarate <zaratedev@gmail.com>
2017-10-12 15:52:32 -0500

    Configuration and creating layout

---
+++ resources/views/template/layout.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }} | @yield('title')</title>
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- fonts -->
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic">
+    <!-- Material icons -->
+    <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
+    <!-- CSS Style -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+    </script>
+
+    @yield('style')
+  </head>
+  <body>
+    <div class="app">
+      @yield('content')
+    </div>
+
+    <script src="{{ asset('js/apps.js')}}" charset="utf-8"></script>
+  </body>
+</html>
