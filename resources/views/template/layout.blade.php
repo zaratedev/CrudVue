c624afb8d623e65bdd972849a3d02012efb51bf7
Jonathan Zarate <zaratedev@gmail.com>
2017-10-12 17:51:49 -0500

    Creating template home. where include Vue.

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -29,6 +29,7 @@
       @yield('content')
     </div>
 
-    <script src="{{ asset('js/apps.js')}}" charset="utf-8"></script>
+    <script src="{{ asset('js/app.js')}}" charset="utf-8"></script>
+    @yield('script')
   </body>
 </html>
